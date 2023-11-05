afdbeb78e3abb9ddcacd6f33ea8eee3948545c00
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2023-11-05 21:38:19 +0530

    transaction added

--- app.py
+++ app.py
@@ -21,15 +21,24 @@ def get_token():
     data = request.json
     user_name = data.get('userName')
     pin = data.get('pin')
-    result = sf.query(f"SELECT Id,Paarai_Id__c,Name,Pin__c FROM Member__c WHERE UserName__c='{user_name}'")
+    if(data.get('isPaarai') == True):
+        result = sf.query(f"SELECT Id,Paarai_Id__c,Name,Pin__c FROM Member__c WHERE UserName__c='{user_name}'")
+    else:
+        result = sf.query(f"SELECT Id,Member_Id__c,Name,Pin__c FROM Group_Member__c WHERE UserName__c='{user_name}'")
     obj = {}
     if result['records']:
         member = result['records'][0]
         if pin == member['PIN__c']:
-            payload = {
-                'memberId': member['Id'],
-                'paaraiId': member['Paarai_Id__c']
-            }
+            if(data.get('isPaarai') == True):
+                payload = {
+                    'memberId': member['Id'],
+                    'paaraiId': member['Paarai_Id__c']
+                }
+            else:
+                payload = {
+                    'Id': member['Id'],
+                    'memberId': member['Member_Id__c'],
+                }
             obj['Name'] = member['Name']
             token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
             obj['token'] = token
@@ -50,9 +59,12 @@ def get_member_info():
 
     try:
         decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        member_id = decoded['memberId']
-        result = sf.query(f"SELECT Id, Name, Work__c, Location__c, Paarai_Id__c, PIN__c, Date_of_Birth__c, Is_Approved__c, Phone_Number__c, UserName__c, Position__c FROM Member__c WHERE Id = '{member_id}'")
-        
+        if(data.get('isPaarai') == True):
+            member_id = decoded['memberId']
+            result = sf.query(f"SELECT Id, Name, Work__c, Location__c, Paarai_Id__c, PIN__c, Date_of_Birth__c, Is_Approved__c, Phone_Number__c, UserName__c, Position__c FROM Member__c WHERE Id = '{member_id}'")
+        else:
+            member_id = decoded['Id']
+            result = sf.query(f"SELECT Id, Name, Aadhar_Number__c, Address__c, Joining_Date__c, Member_Id__c, Mobile__c, Username__c, Position__c FROM Group_Member__c WHERE Id = '{member_id}'")
         if result['records']:
             obj['data'] = result['records'][0]
         else:
@@ -73,8 +85,10 @@ def get_member_list():
 
     try:
         decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        result = sf.query("SELECT Id, Name, Work__c, Paarai_Id__c, Phone_Number__c, UserName__c, Position__c, Location__c FROM Member__c WHERE Is_Approved__c = true ORDER BY Paarai_Id__c ASC")
-        
+        if(data.get('isPaarai') == True):
+            result = sf.query("SELECT Id, Name, Work__c, Paarai_Id__c, Phone_Number__c, UserName__c, Position__c, Location__c FROM Member__c WHERE Is_Approved__c = true ORDER BY Paarai_Id__c ASC")
+        else:
+            result = sf.query("SELECT Id, Name, Member_Id__c, Position__c FROM Group_Member__c ORDER BY Member_Id__c ASC")
         if result['records']:
             obj['data'] = result['records']
         else:
@@ -127,6 +141,25 @@ def get_news():
         obj['message'] = str(err)
 
     return jsonify(obj)
+
+@app.route('/getTransaction', methods=['POST'])
+def get_Transactions():
+    token = request.json.get('token')
+    obj = {'isError': False}
+    try:
+        decoded = jwt.decode(token, 'FLEKNNIRQSQ', algorithms=['HS256'])
+        member_id = decoded['Id']
+        result = sf.query(
+            f"SELECT Id, Name, Type__c, Month__c, Date__c, Description__c, Amount__c, Group_Account__r.Total_Amount__c FROM Group_Transaction__c WHERE Group_Member__c = '{member_id}' AND Group_Account__r.Account_Type__c = 'Savings Account' ORDER BY CreatedDate DESC"
+        )
+
+        obj['data'] = result.get('records')
+    except Exception as err:
+        obj['isError'] = True
+        obj['message'] = str(err)
+
+    return jsonify(obj)
+
 
 @app.route('/saveNews', methods=['POST'])
 def save_news():
